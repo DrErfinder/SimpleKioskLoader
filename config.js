5d2b2732d0607a6abff11a7d905d853e766af765
DrErfinder <46953356+DrErfinder@users.noreply.github.com>
2019-10-31 13:27:31 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// Config
+let lang = "auto", // Language: auto for Automatic, en for English, de for German
+    URL = 'http://localhost/', // Website to open after succesfull 'ping'
+    imgTest = 'test.ico', // picture to ping (the smaler the better), gets appended to URL
+    LoadInterval = 1000, // Time after witch it trys to connect to a Server
+    ErrorTimeout = 10000; // Time after witch an error ist displayed.
