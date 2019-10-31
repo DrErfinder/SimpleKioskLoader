26b123d481edd465f8c642aadc2c5d071d644d32
DrErfinder <46953356+DrErfinder@users.noreply.github.com>
2019-10-31 13:27:54 +0100

    Create translations.js

---
+++ translations.js
@@ -0,0 +1,16 @@
+let translations = {
+    "en": {
+        "title": "Waiting for Network",
+        "lang_connecting": "Establishing network connection",
+        "lang_wait": "Please be patient...",
+        "lang_error": "Can't reach Server!",
+        "lang_retry": "We'll keep trying..."
+    },
+    "de": {
+        "title": "Auf Netzwerk warten",
+        "lang_connecting": "Netzwerkverbindung wird hergestellt",
+        "lang_wait": "Bitte warten...",
+        "lang_error": "Server nicht erreichbar!",
+        "lang_retry": "Wir probieren es weiter..."
+    }
+}
